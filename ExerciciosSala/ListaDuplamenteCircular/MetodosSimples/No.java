ecc9f7c45f61cd8cfa83fc8630963ee14ecd9f0d
Eugênio Victor Erbs <eugenio.job1@gmail.com>
2025-05-23 11:42:20 -0300

    Add files via upload

---
+++ ExerciciosSala/ListaDuplamenteCircular/MetodosSimples/No.java
@@ -0,0 +1,40 @@
+package ListaCircular;
+
+public class No<T> {
+    private T info;
+    private No<T> anterior;
+    private No<T> proximo;
+
+    public No (T info) {
+        this.info = info;
+        this.anterior = null;
+        this.proximo = null;
+
+    }//construtor
+
+    public T getInfo () {
+        return info;
+    }
+
+    public void setInfo (T valorParaOInfo) {
+        this.info = valorParaOInfo;
+    }
+
+    public No<T> getAnterior () {
+        return anterior;
+    }
+    
+    public void setAnterior (No<T> anteriorV) {
+        this.anterior = anteriorV;
+    }
+
+    public No<T> getProximo () {
+        return proximo;
+    }
+    
+    public void setProximo (No<T> proximoV) {
+        this.proximo = proximoV;
+    }
+
+
+}//class
